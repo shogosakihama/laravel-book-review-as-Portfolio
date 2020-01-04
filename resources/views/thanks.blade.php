35c4cb80f235b1759786cb5f144b3f94e1494e26
Shogo Sakihama <goshalways@gmail.com>
2020-01-05 01:40:48 +0900

    0105

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+
+  <div class="row">
+      <h1>お問い合せ</h1>
+  </dv>
+  <div class="row">
+      <p>お問い合せありがとうございました</p>
+  </div>
+  <div class="row">
+      <p>ご入力いただいた内容は正しく送信されました。</p>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -32,17 +32,9 @@ Route::get('/article/test','ArticleController@test');
 Route::post('/article/{id}/likes','LikeController@store')->name('likes.like');
 Route::delete('/article/{id}/unlikes','LikeController@destroy')->name('likes.unlike');
 Route::post('/article/{id}/showFlash','LikeController@showFlash')->name('likes.showFlash');
-// Route::group(['middleware'=>'auth'],function(){
-//   Route::group(['prefix'=>'article/{id}'],function(){
-//       Route::post('likes','LikeController@store')->name('likes.like');
-//       Route::post('likes','LikeController@destroy')->name('likes.unlike');
-//   });
-// });
 
 Route::get('/article/{id}','ArticleController@show')->name('article.show');
 Route::delete('/article/{id}','ArticleController@destroy')->name('article.delete');
-
-
 
 Auth::routes();
 
@@ -54,5 +46,9 @@ Route::get('auth/twitter/callback', 'Auth\TwitterController@handleProviderCallba
 // ログアウトURL
 Route::get('auth/twitter/logout', 'Auth\TwitterController@logout');
 
+//お問合わせ
+Route::get('/mailForm','ContactController@index')->name('contact');
+Route::post('/contact/confirm','ContactController@confirm')->name('confirm');
+Route::post('/contact/sent','ContactController@sent')->name('sent');
 
 Route::get('/home', 'HomeController@index')->name('home');
